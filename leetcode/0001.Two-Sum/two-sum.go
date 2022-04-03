2bb55e0b737e7137b26574361e6ca95ad66b38af
Sergey Suvorov <sergey.personal@gmail.com>
2022-04-03 18:39:29 +0300

    0001 Two Sum

---
+++ leetcode/0001.Two-Sum/two-sum.go
@@ -0,0 +1,28 @@
+package leetcode
+
+//Complecity O(n)
+// Runtime: 14 ms, faster than 42.80% of Go online submissions for Two Sum.
+// Memory Usage: 4.3 MB, less than 53.37% of Go online submissions for Two Sum.
+func TwoSum(nums []int, target int) []int {
+	buf := make(map[int]int)
+	for i, v := range nums {
+		if idx, ok := buf[target-v]; ok {
+			return []int{idx, i}
+		}
+		buf[v] = i
+	}
+	return []int{}
+}
+
+//Complecity O(n^2)
+func TwoSumVersion1(nums []int, target int) []int {
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+
+	return []int{}
+}
---
+++ leetcode/0001.Two-Sum/two-sum_test.go
@@ -0,0 +1,49 @@
+package leetcode_test
+
+import (
+	"reflect"
+	"testing"
+
+	leetcode "github.com/SuvorovSergey/go-leetcode/leetcode/0001.Two-Sum"
+)
+
+func TestTwoSum(t *testing.T) {
+	type args struct {
+		nums   []int
+		target int
+	}
+
+	type test struct {
+		name     string
+		args     args
+		expected []int
+	}
+
+	tests := []test{
+		{
+			name: "test1",
+			args: args{
+				nums:   []int{2, 7, 11, 15},
+				target: 9,
+			},
+			expected: []int{0, 1},
+		},
+		{
+			name: "test2",
+			args: args{
+				nums:   []int{3, 2, 4},
+				target: 6,
+			},
+			expected: []int{1, 2},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			res := leetcode.TwoSum(tt.args.nums, tt.args.target)
+			if !reflect.DeepEqual(tt.expected, res) {
+				t.Errorf("twoSum() = %v, expected %v", res, tt.expected)
+			}
+		})
+	}
+}
