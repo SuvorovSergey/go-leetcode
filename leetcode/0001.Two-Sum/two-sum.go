f0be018142be894ab317940eb7f34b8afb1954da
Sergey Suvorov <sergey.personal@gmail.com>
2022-04-03 18:41:09 +0300

    fix comment

--- leetcode/0001.Two-Sum/two-sum.go
+++ leetcode/0001.Two-Sum/two-sum.go
@@ -1,6 +1,6 @@
 package leetcode
 
-//Complecity O(n)
+// Complecity O(n)
 // Runtime: 14 ms, faster than 42.80% of Go online submissions for Two Sum.
 // Memory Usage: 4.3 MB, less than 53.37% of Go online submissions for Two Sum.
 func TwoSum(nums []int, target int) []int {
@@ -14,7 +14,7 @@ func TwoSum(nums []int, target int) []int {
 	return []int{}
 }
 
-//Complecity O(n^2)
+// Complecity O(n^2)
 func TwoSumVersion1(nums []int, target int) []int {
 	for i := 0; i < len(nums); i++ {
 		for j := i + 1; j < len(nums); j++ {
