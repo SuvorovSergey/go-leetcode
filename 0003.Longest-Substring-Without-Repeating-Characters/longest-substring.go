2337359453c927ea716402525cff8004cce8d07c
Sergey Suvorov <sergey.personal@gmail.com>
2022-04-04 09:27:01 +0300

    Longest Substring Without Repeating Characters

--- 0003.Longest-Substring-Without-Repeating-Characters/longest-substring.go
+++ 0003.Longest-Substring-Without-Repeating-Characters/longest-substring.go
@@ -1,7 +1,9 @@
 package longestsubstring
 
-// Runtime: 16 ms, faster than 41.58% of Go online submissions 
-// Memory Usage: 3.1 MB, less than 57.39% of Go online submissions 
+// Implementation of Sliding Window (https://www.geeksforgeeks.org/window-sliding-technique/)
+
+// Runtime: 16 ms, faster than 41.58% of Go online submissions
+// Memory Usage: 3.1 MB, less than 57.39% of Go online submissions
 func LengthOfLongestSubstring(s string) int {
 	m := make(map[byte]int)
 	longest, left, right := 0, 0, 0
